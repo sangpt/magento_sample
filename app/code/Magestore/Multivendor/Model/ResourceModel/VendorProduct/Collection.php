515d5f8c1721380aa9fa172855bf9983af1c9e1e
Kenny Pham <kenny@trueplus.vn>
2018-09-13 15:25:10 +0700

    done bai 2

---
+++ app/code/Magestore/Multivendor/Model/ResourceModel/VendorProduct/Collection.php
@@ -0,0 +1,17 @@
+<?php
+namespace Magestore\Multivendor\Model\ResourceModel\VendorProduct;
+/**
+* Class Collection
+* @package Magestore\Multivendor\Model\ResourceModel\Vendor
+*/
+class Collection extends \Magento\Framework\Model\ResourceModel\Db\Collection\AbstractCollection
+{
+    protected $_idFieldName = 'vendor_product_id';
+    public function _construct(){
+        parent::_construct();
+        $this->_init(
+            'Magestore\Multivendor\Model\VendorProduct',
+            'Magestore\Multivendor\Model\ResourceModel\VendorProduct'
+        );
+    }
+}
